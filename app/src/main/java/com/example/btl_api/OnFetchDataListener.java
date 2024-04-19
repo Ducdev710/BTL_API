4efe1877de557fe133b687bf9b6b9fb695757439
Ducdev710 <ducvuminh0710@gmail.com>
2024-04-19 23:01:48 +0700

    lam den 50:09 phut

---
+++ app/src/main/java/com/example/btl_api/OnFetchDataListener.java
@@ -0,0 +1,10 @@
+package com.example.btl_api;
+
+import com.example.btl_api.Models.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse> {
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
---
+++ app/src/main/java/com/example/btl_api/SelectListener.java
@@ -0,0 +1,8 @@
+package com.example.btl_api;
+
+import com.example.btl_api.Models.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+
+}
